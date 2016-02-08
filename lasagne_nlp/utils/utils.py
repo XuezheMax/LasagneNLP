0317826ebd0d3cba7d5a010c3e242fb2a67ddf76
Max Ma <xuezhe.ma@gmail.com>
2016-02-08 17:55:55 -0500

    bug

--- lasagne_nlp/utils/utils.py
+++ lasagne_nlp/utils/utils.py
@@ -6,7 +6,7 @@ import numpy as np
 import lasagne
 from gensim.models.word2vec import Word2Vec
 import gzip
-import theano.config.floatX as floatX
+import theano
 
 
 def get_logger(name, level=logging.INFO, handler=sys.stdout,
@@ -45,7 +45,7 @@ def load_word_embedding_dict(embedding, embedding_path, logger):
                     embedd_dim = len(tokens) - 1
                 else:
                     assert(embedd_dim + 1 == len(tokens))
-                embedd = np.empty([1, embedd_dim], dtype=floatX)
+                embedd = np.empty([1, embedd_dim], dtype=theano.config.floatX)
                 embedd[:] = tokens[1:]
                 embedd_dict[tokens[0]] = embedd
         return embedd_dict, embedd_dim
