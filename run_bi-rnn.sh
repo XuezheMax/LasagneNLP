d6a18e2ea1851114a22738c0cffab647b31aafb0
Max Ma <xuezhe.ma@gmail.com>
2016-02-15 17:31:16 -0500

    64 --> 32

--- run_bi-rnn.sh
+++ run_bi-rnn.sh
@@ -1,4 +1,4 @@
-THEANO_FLAGS='floatX=float64' python bi_rnn.py --fine_tune --embedding senna --oov embedding --update momentum \
+THEANO_FLAGS='floatX=float32' python bi_rnn.py --fine_tune --embedding senna --oov embedding --update momentum \
  --batch_size 10 --num_units 100 --grad_clipping 3 --gamma 1e-6 --regular dima \
  --train "data/POS-penn/wsj/split1/wsj1.train.original" --dev "data/POS-penn/wsj/split1/wsj1.dev.original" --test "data/POS-penn/wsj/split1/wsj1.test.original" \
  --embedding_dict "data/senna/senna.50d.gz"
