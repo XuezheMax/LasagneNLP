e8664a0005895dd53efcf1e2482435364fbcec26
Max Ma <xuezhe.ma@gmail.com>
2016-03-05 15:12:57 -0500

    run bi-lstm-cnn.sh

--- run_bi-lstm-cnn.sh
+++ run_bi-lstm-cnn.sh
@@ -1,4 +1,4 @@
-THEANO_FLAGS='floatX=float32' python bi_lstm_cnn.py --fine_tune --embedding glove --oov embedding --update momentum \
- --batch_size 10 --num_units 100 --num_filters 20 --grad_clipping 3 --gamma '1e-6' --regular none \
- --train "data/POS-penn/wsj/split1/wsj1.train.original" --dev "data/POS-penn/wsj/split1/wsj1.dev.original" --test "data/POS-penn/wsj/split1/wsj1.test.original" \
- --embedding_dict "data/glove/glove.6B/glove.6B.100d.gz"
+THEANO_FLAGS='floatX=float32,device=gpu' python bi_lstm_cnn.py --fine_tune --embedding glove --oov embedding --update momentum \
+ --batch_size 10 --num_units 100 --num_filters 20 --learning_rate 0.1 --grad_clipping 3 --gamma '1e-6' --regular dropout \
+ --train "data/conll2003/eng.train.bioes.conll" --dev "data/conll2003/eng.dev.bioes.conll" --test "data/conll2003/eng.test.bioes.conll" \
+ --embedding_dict "data/glove/glove.6B/glove.6B.100d.gz" --output_prediction --patience 5
