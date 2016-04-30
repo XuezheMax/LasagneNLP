ba4afb157cf0a732da334c0881a2dd0c0614e805
Max Ma <xuezhem@cs.cmu.edu>
2016-04-30 11:26:11 -0700

    Update run_cifar100.sh

--- run_cifar100.sh
+++ run_cifar100.sh
@@ -1,2 +1,3 @@
-THEANO_FLAGS='floatX=float32' python cifar100.py --num_epochs 1000 --batch_size 128 --decay_rate 0.005 \
- --delta 0.0 --regular l2 --gamma 1e-3 --patience 5
+THEANO_FLAGS='floatX=float32' python cifar100.py --num_epochs 1000 --batch_size 100 --decay_rate 0.005 \
+ --momentum0 0.5 --momentum1 0.95 --momentum_type normal \
+ --delta 0.0 --regular none --gamma 1e-3 --patience 5
