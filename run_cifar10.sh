30f363201132b92b3ce115766cecbb382637ac95
Max Ma <xuezhem@cs.cmu.edu>
2016-05-01 05:17:03 -0700

    Update run_cifar10.sh

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -1,3 +1,4 @@
-THEANO_FLAGS='floatX=float32' python cifar10.py --num_epochs 1000 --batch_size 100 --decay_rate 0.005 \
+THEANO_FLAGS='floatX=float32' python cifar10.py --num_epochs 1000 --batch_size 100 \
+ --learning_rate_cnn 0.001 --learning_rate_dense 0.1 --decay_rate 0.005 \
  --momentum0 0.5 --momentum1 0.95 --momentum_type normal \
  --delta 0.0 --regular none --gamma 1e-3 --patience 5
