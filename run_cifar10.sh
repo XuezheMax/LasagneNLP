b8ae950191bd83b19cfad8d5da23ccbd62908b11
Max Ma <xuezhe.ma@gmail.com>
2016-05-11 18:04:58 -0400

    add experiments

--- run_cifar10.sh
+++
@@ -1,4 +0,0 @@
-THEANO_FLAGS='floatX=float32' python cifar10.py --num_epochs 1000 --batch_size 100 \
- --learning_rate_cnn 0.001 --learning_rate_dense 0.1 --decay_rate 0.005 \
- --momentum0 0.5 --momentum1 0.95 --momentum_type normal \
- --delta 0.0 --regular none --gamma 1e-3 --patience 5
--- run_mnist.sh
+++
@@ -1,3 +0,0 @@
-THEANO_FLAGS='floatX=float32' python mnist.py --depth 3 --num_units 1024 --activation rectify \
- --num_epochs 1000 --batch_size 100 --learning_rate 0.1 --decay_rate 0.05 --update momentum \
- --delta 0.0 --regular none --gamma 1e-6 --patience 5
