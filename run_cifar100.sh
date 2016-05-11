81ee5c45b3bf171e85f78d4a96ff2042e05f0956
Max Ma <xuezhe.ma@gmail.com>
2016-05-11 18:00:20 -0400

    change run shell

--- run_cifar100.sh
+++ run_cifar100.sh
@@ -1,4 +1,4 @@
-THEANO_FLAGS='floatX=float32' python cifar100.py --num_epochs 1000 --batch_size 100 \
- --learning_rate_cnn 0.001 --learning_rate_dense 0.1 --decay_rate 0.005 \
- --momentum0 0.5 --momentum1 0.95 --momentum_type normal \
- --delta 0.0 --regular none --gamma 1e-3 --patience 5
+THEANO_FLAGS='floatX=float32' python cifar100.py --num_epochs 2000 --batch_size 100 \
+ --learning_rate_cnn 0.001 --decay_rate_cnn 0.005 --learning_rate_dense 0.05 --decay_rate_dense 0.01 \
+ --momentum0 0.8 --momentum1 0.95 --momentum_type nesterov \
+ --delta 0.0 --regular l2 --gamma 1e-3 --patience 5
