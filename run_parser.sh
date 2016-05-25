6382bf52645e6dfbef74ee74acc644d530809a6b
Max Ma <xuezhe.ma@gmail.com>
2016-05-25 14:49:47 -0400

    small change

--- run_parser.sh
+++ run_parser.sh
@@ -1,4 +1,4 @@
-THEANO_FLAGS='floatX=float32' python parser.py --mode parse --embedding senna --update momentum \
+THEANO_FLAGS='floatX=float32' python parser.py --mode parse --embedding random --update momentum \
  --batch_size 1 --num_units 200 --num_filters 30 --momentum 0.9 --grad_clipping 5 --regular none \
  --learning_rate_bottom 0.01 --decay_rate_bottom 0.05 --learning_rate_top 0.002 --decay_rate_top 0.05 \
  --train "data/PTB3.0/PTB3.0-Penn2Malt/tmp.original" --dev "data/PTB3.0/PTB3.0-Penn2Malt/tmp.original" --test "data/PTB3.0/PTB3.0-Penn2Malt/tmp.original" \
