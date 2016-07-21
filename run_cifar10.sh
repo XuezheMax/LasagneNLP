ec7435b0e86cd23555c0e8c20d8f28ad6c63035b
Max Ma <xuezhe.ma@gmail.com>
2016-07-21 10:35:58 -0400

    max

--- run_cifar10.sh
+++
@@ -1,4 +0,0 @@
-THEANO_FLAGS='floatX=float32' python cifar10.py --num_epochs 1000 --batch_size 100 \
- --learning_rate_cnn 0.001 --learning_rate_dense 0.1 --decay_rate 0.005 \
- --momentum0 0.5 --momentum1 0.95 --momentum_type normal \
- --delta 0.0 --regular l2 --gamma 1e-3 --mc 100 --batch_mc 50 --patience 5
--- run_mnist.sh
+++
@@ -1,3 +0,0 @@
-THEANO_FLAGS='floatX=float32' python mnist.py --depth 3 --num_units 1024 --activation rectify \
- --num_epochs 1000 --batch_size 200 --learning_rate 0.1 --decay_rate 0.025 --update momentum \
- --delta 0.0 --regular none --gamma 1e-6 --mc 100 --patience 5
